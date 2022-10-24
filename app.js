603bfeec3da29cf015588425e356da33bc9350c2
mziatvaradze <113588110+mziatvaradze@users.noreply.github.com>
2022-10-24 17:27:14 +0400

    jvs

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('hello') 
+let myName='mzia';
+let myHobby='reading';
+let yearOfBirth=1995;
+let currentYear=2022;
+
+const course="front-end";
+const startMonth=9;
+let myId;
+const myNull=null;
+let isEmployed=true;
+
+const age= currentYear-yearOfBirth;
+
+console.log(age)
+ const newText= "my name is" + myName +", i am "+age+"old and my hobby is"+myHobby;
+ const newText2=' my name is ${myName}, i am ${age} old and my hobby is ${myHobby}';
